0097f02900e4502cb051194ad7de04945f00d450
Ronish Katuwal <ronishkatuwal00@gmail.com>
2025-06-30 18:34:26 +0545

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-7xl font-bold text-red-700">Morphiq</h1>
-      <div className="flex-center">
-        <div className="w-[100px] h-[100px] bg-red-500"></div>
-        <div className="w-[100px] h-[100px] bg-red-500"></div>
-        <div className="w-[100px] h-[100px] bg-red-500"></div>
-        <div className="w-[100px] h-[100px] bg-red-500"></div>
-      </div>
     </div>
   );
 }
