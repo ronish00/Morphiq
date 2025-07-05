6f432f5010a81c3d9822a6601dd064a8b89b5aff
Ronish Katuwal <ronishkatuwal00@gmail.com>
2025-07-05 21:13:24 +0545

    duplicate component removed

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -48,7 +48,7 @@ const Sidebar = () => {
               })}
             </ul>
 
-            <ul className="sidebar-nav_elements" >
+            <ul className="sidebar-nav_elements">
               {navLinks.slice(6).map((link) => {
                 const isActive = link.route === pathname;
                 return (
