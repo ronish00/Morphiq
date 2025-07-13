4f771f8037e5fc009d669f189d7982eedc14725e
Ronish Katuwal <ronishkatuwal00@gmail.com>
2025-07-13 21:41:49 +0545

    middleware.ts changed

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -27,8 +27,8 @@ export async function POST(req: NextRequest) {
         clerkId: id,
         email: email_addresses[0].email_address,
         username: username!,
-        firstName: first_name,
-        lastName: last_name,
+        firstName: first_name ?? "",
+        lastName: last_name ?? "",
         photo: image_url,
       };
 
@@ -54,8 +54,8 @@ export async function POST(req: NextRequest) {
       const { id, image_url, first_name, last_name, username } = evt.data;
 
       const user = {
-        firstName: first_name,
-        lastName: last_name,
+        firstName: first_name ?? "",
+        lastName: last_name ?? "",
         username: username!,
         photo: image_url,
       };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/webhooks(.*)"]);
-
-export default clerkMiddleware(async(auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
