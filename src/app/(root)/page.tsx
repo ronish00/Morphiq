82879d3ae68f320d9826f2d5cda999af4a1734fd
Ronish Katuwal <ronishkatuwal00@gmail.com>
2025-08-30 18:58:40 +0545

    changed heading

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -15,7 +15,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with Imaginify
+          Unleash Your Creative Vision with Morphiq
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
